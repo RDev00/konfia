0f752626d1d4df224ee2b5ca3f1fd627e1acb853
RavexCode <rd77extragd@gmail.com>
2025-10-09 01:10:55 +0000

    Hooks terminadas & bugfix
    Hook de login arreglada y comentada y hook de register agregada, todo listo para empezar accounts.

--- src/hooks/register.auth.js
+++ src/hooks/register.auth.js
@@ -1,25 +1,15 @@
-//Creamos la funcion para el fetch
 async function fetchFunction(json) {
-  //Declaramos el error de mensaje de usuario
   const errorMessage = { "error" : "Ha ocurrido un error al iniciar sesión" };
-
-  //Usamos try-catch por si llega a haber un error entre la conexion
   try {
-    //Hacemos el fetch
-    const res = await fetch('http://localhost:2345/users/login', {
-      //Declaramos el metodo
+    const res = await fetch('http://localhost:2345/users/register', {
       method: "POST",
-      //Los headers (Como no require auth, no la agregamos)
       headers: { "Content-Type" : "application/json" },
-      //Agregamos el contenido en JSON hecho texto
       body: JSON.stringify(json)
     });
 
-    //Si es valida la respuesta retornamos los datos
     if(res.ok) {
       return await res.json();
     } else {
-    //Sino, retornamos error
       return errorMessage;
     }
   } catch (error) {
@@ -28,36 +18,29 @@ async function fetchFunction(json) {
   }
 };
 
-//Declaramos la funcion del login
-async function login(storename, password) {
-  //Creamos el JSON
+async function register(storename, password) {
   const newJson = {
+    "usertag" : usertag,
     "store" : storename,
     "password" : password
   };
 
-  //Hacemos la respuesta
   const res = await fetchFunction(newJson);
 
-  //Declaramos la data
   const data = {
     "response": false,
     "message": ""
   };
 
-  //Si es error lo indicamos
   if(res.error) {
     data.response = false;
     data.message = res.error;
   } else {
-    //Sino, mandamos mensaje de exito
     data.response = true;
     data.message = "Sesion iniciada con exito";
   }
 
-  //Retornamos la data
   return data;
 }
 
-//Exportamos el login
-export default login;
+export default register;
