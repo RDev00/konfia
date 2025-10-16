4923200da7760695b6bedccd53219358f65808b2
RDev00 <rd77extragd@gmail.com>
2025-10-16 12:10:50 -0600

    Bugfix
    Una bugfix algo simple debido a errores en el deploy

--- routes/data.controller.js
+++ routes/data.controller.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const RouterModel = require('../models/router.model');
 const UserModel = require('../models/user.model');
+
+//Dar reviews a usuarios y tiendas, como poder eliminarlas
+//Actualizar la calificacion del usuario y las reviews que tiene este mismo
+
+
 
 module.exports = router;
